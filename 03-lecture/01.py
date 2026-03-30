82ef78467e2f1fb9dd7d884dd2d25aa9a3fcec81
Laiba Farooq <laibafarooqcs@gmail.com>
2026-03-30 20:31:32 +0500

    3nd lecture 1 pro

---
+++ 03-lecture/01.py
@@ -0,0 +1,9 @@
+traffic_light = "Red"
+
+# Check the condition
+if traffic_light == "Green":
+    print("You can GO! ")  # If the light is green, cars can go
+elif traffic_light == "Red":
+    print("You must STOP! ")  # If the light is red, cars must stop
+else:
+    print("Slow down and PREPARE TO STOP! ")
