32b76207bf7843aa566a4031696428ebbfcbc223
Laiba Farooq <laibafarooqcs@gmail.com>
2026-04-01 22:48:41 +0500

    lecture 4 pro 1

---
+++ 04-lecture/01.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+
+
+prices = [10, 25, 8, 15, 30]
+highest_price = max(prices)
+print("The highest price on the menu is:", highest_price)
+
+ratings = [4.5, 3.9, 5.0, 4.2, 4.8]
+sorted_ratings = sorted(ratings)
+print("The restaurant ratings from lowest to highest:", sorted_ratings)
