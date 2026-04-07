1ddeca3254f4493bfd7ec3479a424cd63c638e94
Laiba Farooq <laibafarooqcs@gmail.com>
2026-04-07 22:49:47 +0500

    lecture 9 pro 1

---
+++ 09-lecture/01.py
@@ -0,0 +1,10 @@
+file = open('filename.txt', 'mode')
+# Perform operations on the file
+file.close()
+
+
+# Opening a file and closing it properly
+file = open('example.txt', 'w')
+file.write('Hello, this is a sample file!')
+file.close()
+
