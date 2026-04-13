8e8a7220a9c26c3978662e3b646328bf86fd5d00
Laiba Farooq <laibafarooqcs@gmail.com>
2026-04-14 00:48:59 +0500

    lecture 11 pro 3

---
+++ 11-lecture/03.py
@@ -0,0 +1,11 @@
+class House:
+    # The constructor (__init__) method initializes the attributes of the object
+    def __init__(self, color, size):
+        self.color = color  # Attribute to store the color of the house
+        self.size = size    # Attribute to store the size of the house
+
+# Create an object of the House class with specific color and size
+my_house = House("blue", "large")
+
+# Print the color and size of the house object
+print(my_house.color, my_house.size)
